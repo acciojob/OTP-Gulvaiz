799d21ca2c7a5610f6f1dc84fa9fddc3eb726861
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-08-01 13:12:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your JS code here. If required.
+  let inputFields = document.querySelectorAll(".code")
+         inputFields.forEach((input, index) => {
+                input.addEventListener("input", (event) => {
+                      let value = event.target.value
+                      if(value){
+                             if(index < inputFields.length - 1){
+                                  inputFields[index+1].focus()
+                             }
+                      }
+                })
+         })
 
+         inputFields.forEach((input,index) => {
+                    input.addEventListener("keydown", (event) => {
+                        if(event.key === "Backspace"){
+                            if(input.value === ""){
+                           if(index > 0){
+                                inputFields[index-1].focus()
+                           }
+                        }
+                    }
+                    })
+         })
+    
