77673935cb58eb9b6271af5467674549a8bb1285
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-08-02 20:19:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-const inputFields = document.querySelectorAll(".code");
+const inputFields = document.querySelectorAll(“.code”);
 
 inputFields.forEach((input, index) => {
-  input.addEventListener("input", () => {
-    if (input.value !== "" && index < inputFields.length - 1) {
-      input.classList.remove("focused");
-      inputFields[index + 1].classList.add("focused");
-      inputFields[index + 1].focus();
-    }
-  });
-
-  input.addEventListener("keydown", (event) => {
-    if (event.key === "Backspace" && input.value === "" && index > 0) {
-      input.classList.remove("focused");
-      inputFields[index - 1].classList.add("focused");
-      inputFields[index - 1].focus();
-    }
-  });
+input.addEventListener(“input”, () => {
+if (input.value !== “” && index < inputFields.length - 1) {
+input.classList.remove(“focused”);
+inputFields[index + 1].classList.add(“focused”);
+inputFields[index + 1].focus();
+}
 });
+
+input.addEventListener(“keydown”, (event) => {
+if (event.key === “Backspace” && input.value === “” && index > 0) {
+input.classList.remove(“focused”);
+inputFields[index - 1].classList.add(“focused”);
+inputFields[index - 1].focus();
+}
+});
+});
+document.getElementById("code-1").focus();
