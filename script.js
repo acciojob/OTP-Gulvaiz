6a57a2e5804d08823ede9369e0216dd6d01ca14c
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-08-02 20:09:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ const inputFields = document.querySelectorAll(".code");
 inputFields.forEach((input, index) => {
   input.addEventListener("input", () => {
     if (input.value !== "" && index < inputFields.length - 1) {
-      input.classList.remove("focused-input");
-      inputFields[index + 1].classList.add("focused-input");
+      input.classList.remove("focused");
+      inputFields[index + 1].classList.add("focused");
       inputFields[index + 1].focus();
     }
   });
 
   input.addEventListener("keydown", (event) => {
     if (event.key === "Backspace" && input.value === "" && index > 0) {
-      input.classList.remove("focused-input");
-      inputFields[index - 1].classList.add("focused-input");
+      input.classList.remove("focused");
+      inputFields[index - 1].classList.add("focused");
       inputFields[index - 1].focus();
     }
   });
