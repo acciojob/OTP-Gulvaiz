5ccd72d0c84f1a0c9e50b1a53448570613773903
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-08-02 19:22:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
 const inputFields = document.querySelectorAll(".code");
 
-// Handle input event
 inputFields.forEach((input, index) => {
   input.addEventListener("input", () => {
     if (input.value !== "" && index < inputFields.length - 1) {
-      input.classList.remove("focused"); // Remove focus class from the current input
-      inputFields[index + 1].classList.add("focused"); // Add focus class to the next input
-      inputFields[index + 1].focus(); // Focus the next input
+      input.classList.remove("focused-input");
+      inputFields[index + 1].classList.add("focused-input");
+      inputFields[index + 1].focus();
     }
   });
-});
 
-// Handle backspace event
-inputFields.forEach((input, index) => {
   input.addEventListener("keydown", (event) => {
     if (event.key === "Backspace" && input.value === "" && index > 0) {
-      input.classList.remove("focused"); // Remove focus class from the current input
-      inputFields[index - 1].classList.add("focused"); // Add focus class to the previous input
-      inputFields[index - 1].focus(); // Focus the previous input
+      input.classList.remove("focused-input");
+      inputFields[index - 1].classList.add("focused-input");
+      inputFields[index - 1].focus();
     }
   });
 });
-
-
