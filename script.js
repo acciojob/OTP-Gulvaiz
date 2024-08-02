1e23b0858ffb40c93246211dbeb9490a6fcabcf5
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-08-02 19:10:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-const inputFields = document.querySelectorAll(".code")
+const inputFields = document.querySelectorAll(".code");
 
+// Handle input event
 inputFields.forEach((input, index) => {
-	input.addEventListener("input",  () => {
-	if(input.value != "" && index < inputFields.length-1){
-	input.classList.remove("focused");
-		inputFields[index+1].classList.add("focused")
-		 inputFields[index+1].focus()
-		
-	}
-	})
-}) 
+  input.addEventListener("input", () => {
+    if (input.value !== "" && index < inputFields.length - 1) {
+      input.classList.remove("focused"); // Remove focus class from the current input
+      inputFields[index + 1].classList.add("focused"); // Add focus class to the next input
+      inputFields[index + 1].focus(); // Focus the next input
+    }
+  });
+});
 
-inputFields.forEach((input , index) => {
-	 input.addEventListener("keydown", (event) => {
-		if (event.key === "Backspace" && input.value === "" && index > 0) {
- input.classList.remove("focused");
- inputFields[index - 1].classList.remove("focused");
-  inputFields[index - 1].focus();
-}
+// Handle backspace event
+inputFields.forEach((input, index) => {
+  input.addEventListener("keydown", (event) => {
+    if (event.key === "Backspace" && input.value === "" && index > 0) {
+      input.classList.remove("focused"); // Remove focus class from the current input
+      inputFields[index - 1].classList.add("focused"); // Add focus class to the previous input
+      inputFields[index - 1].focus(); // Focus the previous input
+    }
+  });
+});
 
-	 })
-})
